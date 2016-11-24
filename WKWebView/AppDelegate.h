bcddb8fbbe59c35c600c131c567a2d0e51c37f34
MyMac <calvin@MyMacdeMac.local>
2016-11-23 18:00:31 -0800

    Initial Commit

---
+++ WKWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebView
+//
+//  Created by MyMac on 2016/11/23.
+//  Copyright © 2016年 hengshuimofangkeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
